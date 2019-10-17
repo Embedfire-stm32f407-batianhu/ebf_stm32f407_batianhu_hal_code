a172bacaace1020ce92313f98dd8b62687b3c95f
hejiawen <hjw0415@utlook.com>
2019-10-17 18:04:38 +0800

    修改为1.5.0hal库

--- 4-新建工程-固件库版本/User/led/bsp_led.c
+++ 4-新建工程-固件库版本/User/led/bsp_led.c
@@ -8,7 +8,7 @@
   ******************************************************************************
   * @attention
   *
-  * 实验平台:野火 STM32 F429 开发板  
+  * 实验平台:野火 STM32 F407 开发板  
   * 论坛    :http://www.firebbs.cn
   * 淘宝    :http://firestm32.taobao.com
   *
@@ -32,7 +32,6 @@ void LED_GPIO_Config(void)
     LED1_GPIO_CLK_ENABLE();
     LED2_GPIO_CLK_ENABLE();
     LED3_GPIO_CLK_ENABLE();
-    LED4_GPIO_CLK_ENABLE();
 
     /*选择要控制的GPIO引脚*/															   
     GPIO_InitStruct.Pin = LED1_PIN;	
@@ -57,15 +56,10 @@ void LED_GPIO_Config(void)
     GPIO_InitStruct.Pin = LED3_PIN;	
     HAL_GPIO_Init(LED3_GPIO_PORT, &GPIO_InitStruct);	
 
-    /*选择要控制的GPIO引脚*/															   
-    GPIO_InitStruct.Pin = LED4_PIN;	
-    HAL_GPIO_Init(LED4_GPIO_PORT, &GPIO_InitStruct);	
 
     /*关闭RGB灯*/
     LED_RGBOFF;
 
-    /*指示灯默认开启*/
-    LED4(ON);
-		
+
 }
 /*********************************************END OF FILE**********************/
--- 4-新建工程-固件库版本/User/main.c
+++ 4-新建工程-固件库版本/User/main.c
@@ -29,7 +29,7 @@ static void SystemClock_Config(void);
   */
 int main(void)
 {
-    /* 系统时钟初始化成216 MHz */
+    /* 系统时钟初始化成168 MHz */
     SystemClock_Config();
     /* 在这里添加你的代码^_^. */
     while(1);  
@@ -38,14 +38,14 @@ int main(void)
 /**
   * @brief  系统时钟配置 
   *            System Clock source            = PLL (HSE)
-  *            SYSCLK(Hz)                     = 180000000
-  *            HCLK(Hz)                       = 180000000
+  *            SYSCLK(Hz)                     = 168000000
+  *            HCLK(Hz)                       = 168000000
   *            AHB Prescaler                  = 1
   *            APB1 Prescaler                 = 4
   *            APB2 Prescaler                 = 2
-  *            HSE Frequency(Hz)              = 12000000
+  *            HSE Frequency(Hz)              = 25000000
   *            PLL_M                          = 25
-  *            PLL_N                          = 360
+  *            PLL_N                          = 336
   *            PLL_P                          = 2
   *            PLL_Q                          = 4
   *            VDD(V)                         = 3.3
@@ -68,7 +68,7 @@ static void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
   RCC_OscInitStruct.PLL.PLLM = 25;
-  RCC_OscInitStruct.PLL.PLLN = 360;
+  RCC_OscInitStruct.PLL.PLLN = 336;
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
   RCC_OscInitStruct.PLL.PLLQ = 4;
   ret = HAL_RCC_OscConfig(&RCC_OscInitStruct);
@@ -78,18 +78,12 @@ static void SystemClock_Config(void)
     while(1) {}
   }
 
-  /* 激活 OverDrive 模式以达到180M频率 */
-  ret =HAL_PWREx_EnableOverDrive();
-   if( ret != HAL_OK)
-  {
-    while(1) {}
-  }
  
   /* 选择PLLCLK作为SYSCLK，并配置 HCLK, PCLK1 and PCLK2 的时钟分频因子 
-	 * SYSCLK = PLLCLK     = 216M
-	 * HCLK   = SYSCLK / 1 = 216M
-	 * PCLK2  = SYSCLK / 2 = 108M
-	 * PCLK1  = SYSCLK / 4 = 54M
+	 * SYSCLK = PLLCLK     = 168M
+	 * HCLK   = SYSCLK / 1 = 168M
+	 * PCLK2  = SYSCLK / 2 = 84M
+	 * PCLK1  = SYSCLK / 4 = 42M
 	 */
   RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
