1070cfadede1bee00f175d2775f059c73da11c4b
hejiawen <hjw0415@utlook.com>
2019-10-18 11:43:38 +0800

    新建串行flash例程，调试成功，可以正常运行

---
+++ 15-串行FLASH文件系统FatFs/User/main.c
@@ -0,0 +1,224 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  fire
+  * @version V1.0
+  * @date    2017-xx-xx
+  * @brief   GPIO输出--使用固件库点亮LED灯
+  ******************************************************************************
+  * @attention
+  *
+  * 实验平台:野火 STM32 F429 开发板 
+  * 论坛    :http://www.firebbs.cn
+  * 淘宝    :http://firestm32.taobao.com
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "stm32f4xx.h"
+#include "./usart/bsp_debug_usart.h"
+#include "./led/bsp_led.h"  
+#include "./flash/bsp_spi_flash.h" 
+#include "ff.h"
+/**
+  ******************************************************************************
+  *                              定义变量
+  ******************************************************************************
+  */
+FATFS fs;													/* FatFs文件系统对象 */
+FIL fnew;													/* 文件对象 */
+FRESULT res_flash;                /* 文件操作结果 */
+UINT fnum;            					  /* 文件成功读写数量 */
+BYTE ReadBuffer[1024]={0};        /* 读缓冲区 */
+BYTE WriteBuffer[] =              /* 写缓冲区*/
+"欢迎使用野火STM32 F407开发板 今天是个好日子，新建文件系统测试文件\r\n";  
+
+/**
+  ******************************************************************************
+  *                                任务函数
+  ******************************************************************************
+  */
+/**
+  * @brief  主函数
+  * @param  无
+  * @retval 无
+  */
+int main(void)
+{
+  SystemClock_Config();
+	/* 初始化LED */
+	LED_GPIO_Config();	
+	LED_BLUE;
+	
+	/* 初始化调试串口，一般为串口1 */
+	 DEBUG_USART_Config();	
+  printf("****** 这是一个SPI FLASH 文件系统实验 ******\r\n");
+  
+	//在外部SPI Flash挂载文件系统，文件系统挂载时会对SPI设备初始化
+	res_flash = f_mount(&fs,"1:",1);
+	
+/*----------------------- 格式化测试 ---------------------------*/  
+	/* 如果没有文件系统就格式化创建创建文件系统 */
+	if(res_flash == FR_NO_FILESYSTEM)
+	{
+		printf("》FLASH还没有文件系统，即将进行格式化...\r\n");
+    /* 格式化 */
+		res_flash=f_mkfs("1:",0,0);							
+		
+		if(res_flash == FR_OK)
+		{
+			printf("》FLASH已成功格式化文件系统。\r\n");
+      /* 格式化后，先取消挂载 */
+			res_flash = f_mount(NULL,"1:",1);			
+      /* 重新挂载	*/			
+			res_flash = f_mount(&fs,"1:",1);
+		}
+		else
+		{
+			LED_RED;
+			printf("《《格式化失败。》》\r\n");
+			while(1);
+		}
+	}
+  else if(res_flash!=FR_OK)
+  {
+    printf("！！外部Flash挂载文件系统失败。(%d)\r\n",res_flash);
+    printf("！！可能原因：SPI Flash初始化不成功。\r\n");
+		while(1);
+  }
+  else
+  {
+    printf("》文件系统挂载成功，可以进行读写测试\r\n");
+  }
+  
+/*----------------------- 文件系统测试：写测试 -----------------------------*/
+	/* 打开文件，如果文件不存在则创建它 */
+	printf("\r\n****** 即将进行文件写入测试... ******\r\n");	
+	res_flash = f_open(&fnew, "1:FatFs读写测试文件.txt",FA_CREATE_ALWAYS | FA_WRITE );
+	if ( res_flash == FR_OK )
+	{
+		printf("》打开/创建FatFs读写测试文件.txt文件成功，向文件写入数据。\r\n");
+    /* 将指定存储区内容写入到文件内 */
+		res_flash=f_write(&fnew,WriteBuffer,sizeof(WriteBuffer),&fnum);
+    if(res_flash==FR_OK)
+    {
+      printf("》文件写入成功，写入字节数据：%d\n",fnum);
+      printf("》向文件写入的数据为：\r\n%s\r\n",WriteBuffer);
+    }
+    else
+    {
+      printf("！！文件写入失败：(%d)\n",res_flash);
+    }    
+		/* 不再读写，关闭文件 */
+    f_close(&fnew);
+	}
+	else
+	{	
+		LED_RED;
+		printf("！！打开/创建文件失败。\r\n");
+	}
+	
+/*------------------- 文件系统测试：读测试 ------------------------------------*/
+	printf("****** 即将进行文件读取测试... ******\r\n");
+	res_flash = f_open(&fnew, "1:FatFs读写测试文件.txt", FA_OPEN_EXISTING | FA_READ); 	 
+	if(res_flash == FR_OK)
+	{
+		LED_GREEN;
+		printf("》打开文件成功。\r\n");
+		res_flash = f_read(&fnew, ReadBuffer, sizeof(ReadBuffer), &fnum); 
+    if(res_flash==FR_OK)
+    {
+      printf("》文件读取成功,读到字节数据：%d\r\n",fnum);
+      printf("》读取得的文件数据为：\r\n%s \r\n", ReadBuffer);	
+    }
+    else
+    {
+      printf("！！文件读取失败：(%d)\n",res_flash);
+    }		
+	}
+	else
+	{
+		LED_RED;
+		printf("！！打开文件失败。\r\n");
+	}
+	/* 不再读写，关闭文件 */
+	f_close(&fnew);	
+  
+	/* 不再使用文件系统，取消挂载文件系统 */
+	f_mount(NULL,"1:",1);
+  
+  /* 操作完成，停机 */
+	while(1)
+	{
+	}
+}
+
+/**
+  * @brief  System Clock Configuration
+  *         The system Clock is configured as follow : 
+  *            System Clock source            = PLL (HSE)
+  *            SYSCLK(Hz)                     = 168000000
+  *            HCLK(Hz)                       = 168000000
+  *            AHB Prescaler                  = 1
+  *            APB1 Prescaler                 = 4
+  *            APB2 Prescaler                 = 2
+  *            HSE Frequency(Hz)              = 8000000
+  *            PLL_M                          = 25
+  *            PLL_N                          = 336
+  *            PLL_P                          = 2
+  *            PLL_Q                          = 7
+  *            VDD(V)                         = 3.3
+  *            Main regulator output voltage  = Scale1 mode
+  *            Flash Latency(WS)              = 5
+  * @param  None
+  * @retval None
+  */
+ void SystemClock_Config(void)
+{
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+
+  /* Enable Power Control clock */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  
+  /* The voltage scaling allows optimizing the power consumption when the device is 
+     clocked below the maximum system frequency, to update the voltage scaling value 
+     regarding system frequency refer to product datasheet.  */
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+  
+  /* Enable HSE Oscillator and activate PLL with HSE as source */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 25;
+  RCC_OscInitStruct.PLL.PLLN = 336;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
+  if(HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    while(1) {};
+  }
+  
+  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2 
+     clocks dividers */
+  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;  
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;  
+  if(HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    while(1) {};
+  }
+
+  /* STM32F405x/407x/415x/417x Revision Z devices: prefetch is supported  */
+  if (HAL_GetREVID() == 0x1001)
+  {
+    /* Enable the Flash prefetch */
+    __HAL_FLASH_PREFETCH_BUFFER_ENABLE();
+  }
+}
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
