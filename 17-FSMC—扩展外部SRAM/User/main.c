5c6eaf396e4d140fc4d1c2124dba9de8df5ab0b2
hejiawen <hjw0415@utlook.com>
2019-10-18 15:28:10 +0800

    修改为1.5.0hal库，修改注释，测试通过

--- 17-FSMC—扩展外部SRAM/User/main.c
+++ 17-FSMC—扩展外部SRAM/User/main.c
@@ -25,7 +25,6 @@
   
 /*绝对定位方式访问SRAM,这种方式必须定义成全局变量*/
 uint8_t testValue __attribute__((at(Bank1_SRAM4_ADDR)));
-
 /**
   * @brief  主函数
   * @param  无
@@ -33,62 +32,48 @@ uint8_t testValue __attribute__((at(Bank1_SRAM4_ADDR)));
   */
 int main(void)
 {
-  /* 系统时钟初始化成216 MHz */
+  /* 系统时钟初始化成168 MHz */
   SystemClock_Config();
   /* LED 端口初始化 */
   LED_GPIO_Config();	 
   /* 初始化串口 */
   DEBUG_USART_Config();
-
    //初始化外部SRAM  
   FSMC_SRAM_Init();
-	
-	printf ( "\r\野火外部 SRAM 测试\r\n" );
-	
+	printf ( "\r\n野火外部 SRAM 测试\r\n" );
   /*蓝灯亮，表示正在读写SRAM测试*/
   LED_BLUE;
-  
   /*对SRAM进行读写测试，检测SRAM是否正常*/
   if(SRAM_Test()==1)
   {
-		//测试正常 绿灯亮
-    LED_GREEN;			  
+    LED_GREEN;//测试正常 绿灯亮			  
   }
 	else
-	{
-		//测试失败 红灯亮
-		LED_RED;
+	{	
+		LED_RED;//测试失败 红灯亮
 	}
 	
 	/*指针方式访问SRAM*/
 	{	
 	 uint32_t temp;
-	
 	 printf("\r\n指针方式访问SRAM\r\n");
 	/*向SRAM写入8位数据*/
 	 *( uint8_t*) (Bank1_SRAM4_ADDR ) = (uint8_t)0xAA;
 	 printf("\r\n指针访问SRAM，写入数据0xAA \r\n");
-
 	 /*从SRAM读取数据*/
 	 temp =  *( uint8_t*) (Bank1_SRAM4_ADDR );
 	 printf("读取数据：0x%X \r\n",temp);
-
 	 /*写/读 16位数据*/
 	 *( uint16_t*) (Bank1_SRAM4_ADDR+10 ) = (uint16_t)0xBBBB;
 	 printf("指针访问SRAM，写入数据0xBBBB \r\n");
-	 
 	 temp =  *( uint16_t*) (Bank1_SRAM4_ADDR+10 );
 	 printf("读取数据：0x%X \r\n",temp);
-
-
 	 /*写/读 32位数据*/
 	 *( uint32_t*) (Bank1_SRAM4_ADDR+20 ) = (uint32_t)0xCCCCCCCC;
 	 printf("指针访问SRAM，写入数据0xCCCCCCCC \r\n");	 
 	 temp =  *( uint32_t*) (Bank1_SRAM4_ADDR+20 );
 	 printf("读取数据：0x%X \r\n",temp);
-
 	}
-	
 	/*绝对定位方式访问SRAM,这种方式必须定义成全局变量*/
 	{
 		testValue = 0xDD;
@@ -96,7 +81,6 @@ int main(void)
 	}
     while(1)
     {
-
     }
 }
 
