ae14fd7467934894bf95c2bc58a70c2ef3e6a8d8
hejiawen <hjw0415@utlook.com>
2019-10-21 09:57:36 +0800

    升级库，修改main.c文件，解决了只显示一路adc的错误

--- ADC—独立模式-多通道-DMA/User/main.c
+++ ADC—独立模式-多通道-DMA/User/main.c
@@ -22,11 +22,12 @@
 #include "./led/bsp_led.h" 
 #include "./adc/bsp_adc.h"
 
-//ADC1转换的电压值通过MDA方式传到SRAM
-extern __IO uint16_t ADC_ConvertedValue;
+// ADC转换的电压值通过MDA方式传到SRAM
+extern __IO uint16_t ADC_ConvertedValue[RHEOSTAT_NOFCHANEL];
 
 // 局部变量，用于保存转换计算后的电压值 	 
-float ADC_Vol; 
+float ADC_ConvertedValueLocal[RHEOSTAT_NOFCHANEL]={0}; 
+
 
 static void Delay(__IO uint32_t nCount)	 //简单的延时函数
 {
@@ -41,18 +42,26 @@ int main(void)
 {   
     /* 配置系统时钟为216 MHz */
     SystemClock_Config();
+
     /* 初始化USART1 配置模式为 115200 8-N-1 */
     DEBUG_USART_Config();
-    Rheostat_Init();    
+
+    Rheostat_Init();
     while (1)
-    {	
-		Delay(0x1fffff);
-        ADC_Vol =(float) ADC_ConvertedValue/4096*(float)3.3; // 读取转换的AD值
-        printf("\r\n The current AD value = 0x%04X \r\n", ADC_ConvertedValue); 
-        printf("\r\n The current AD value = %f V \r\n",ADC_Vol);     
-          
+    {
+		ADC_ConvertedValueLocal[0] =(float) ADC_ConvertedValue[0]/4096*(float)3.3;
+		ADC_ConvertedValueLocal[1] =(float) ADC_ConvertedValue[1]/4096*(float)3.3;
+		ADC_ConvertedValueLocal[2] =(float) ADC_ConvertedValue[2]/4096*(float)3.3;
+		
+		printf("\r\n CH1_PC3 value = %f V \r\n",ADC_ConvertedValueLocal[0]);
+		printf("\r\n CH2_PA4 value = %f V \r\n",ADC_ConvertedValueLocal[1]);
+		printf("\r\n CH3_PA6 value = %f V \r\n",ADC_ConvertedValueLocal[2]);
+		
+		printf("\r\n\r\n");
+		Delay(0xffffff);  
     }   
 }
+
 
 
 /**
